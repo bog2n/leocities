7bfe024228a1378f8ab784dd32f36ca54852bc0f
duckl1ng <bogdan@sanocki.info>
2025-07-15 15:16:39 +0200

    add basic login

---
+++ src/Controller/PanelController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PanelController extends AbstractController
+{
+    #[Route('/panel/', name: 'app_panel')]
+    public function index(): Response
+    {
+        return $this->render('panel/index.html.twig');
+    }
+}
