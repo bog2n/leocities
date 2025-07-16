542fc1dfc79beaf115bf18268bb9061dca5706ed
duckl1ng <bogdan@sanocki.info>
2025-07-16 17:11:05 +0200

    add quota service

---
+++ src/Service/Fs/Exception/QuotaLimitExceeded.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Service\Fs\Exception;
+
+class QuotaLimitExceeded extends \Exception {
+    public function __construct($code = 0, ?Throwable $previous = null) {
+        parent::__construct("Quota limit exceeded", $code, $previous);
+    }
+}
+
---
+++ src/Service/Fs/Quota.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Service\Fs;
+
+use Symfony\Bundle\SecurityBundle\Security;
+use Doctrine\ORM\EntityManagerInterface;
+
+class Quota {
+    public function __construct(
+        private EntityManagerInterface $manager,
+        private Security $security
+    ) {
+    }
+
+    public function add_blocks($num) {
+        $user = $this->security->getUser();
+        if ($user === null) {
+            throw new \Exception("user not found");
+        }
+
+        $conn = $this->manager->getConnection();
+
+        try {
+            $new_quota = $conn->fetchNumeric('UPDATE "user" u
+                SET quota_used = quota_used + ?
+                WHERE id = ?
+                RETURNING quota_used', [
+                    1 => $num,
+                    2 => $user->getId(),
+                ]);
+        } catch (\Exception $e) {
+            // exception thrown, probably quota check failed
+            throw new Exception\QuotaLimitExceeded;
+        }
+
+        return $new_quota[0];
+    }
+
+    public function remove_blocks($num) {
+        $user = $this->security->getUser();
+        if ($user === null) {
+            throw new \Exception("user not found");
+        }
+
+        $conn = $this->manager->getConnection();
+
+        try {
+            $new_quota = $conn->fetchNumeric('UPDATE "user" u
+                SET quota_used = quota_used - ?
+                WHERE id = ?
+                RETURNING quota_used', [
+                    1 => $num,
+                    2 => $user->getId(),
+                ]);
+        } catch (\Exception $e) {
+            // exception thrown, probably freed too much space
+            $conn->fetchNumeric('UPDATE "user" u
+                SET quota_used = 0
+                WHERE id = ?', [
+                    1 => $user->getId(),
+                ]);
+            return 0;
+        }
+
+        return $new_quota[0];
+    }
+}
+
