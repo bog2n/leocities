14e8e1077ab153beee983a7855c2f3acf9075ffb
duckl1ng <bogdan@sanocki.info>
2025-07-17 23:51:11 +0200

    add file listing

--- src/Controller/PanelController.php
+++ src/Controller/PanelController.php
@@ -6,8 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpKernel\Exception as HttpException;
+use Symfony\Component\Filesystem\Path;
+use Symfony\Component\Mime\MimeTypes;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Form\DescriptionType;
+use App\Service\Fs\FileService;
+use App\Repository\InodeRepository;
 
 final class PanelController extends AbstractController
 {
@@ -17,7 +22,8 @@ final class PanelController extends AbstractController
 		EntityManagerInterface $manager
 	): Response
     {
-		$website = $this->getUser()->getWebsite();
+		$user = $this->getUser();
+		$website = $user->getWebsite();
 		$info_form = $this->createForm(DescriptionType::class, $website);
 
 		$info_form->handleRequest($request);
@@ -28,8 +34,42 @@ final class PanelController extends AbstractController
 		}
 
 		return $this->render('panel/index.html.twig', [
-			'user' => $this->getUser(),
+			'user' => $user,
 			'info_form' => $info_form,
+			'root_inode' => $user->getRootInode()->getId(),
 		]);
     }
+
+	#[Route('/panel/file/{id}', methods: ['GET'], name: 'file_get')]
+	public function file_get(
+		FileService $fs,
+		InodeRepository $repository,
+		Request $request,
+		int $id
+	): Response
+	{
+		$inode = $repository->findOneById($id);
+		if ($inode === null) {
+			throw new HttpException\NotFoundHttpException;
+		}
+
+		if ($inode->isDir()) {
+			return $this->render('panel/list.html.twig', [
+				'files' => $fs->list_dir($inode),
+			]);
+		}
+
+        $ext = Path::getExtension($inode->getName());
+        $mimeTypes = new MimeTypes();
+        $mime = $mimeTypes->getMimeTypes($ext);
+        if (count($mime) === 0) {
+            $mime = 'application/octet-stream';
+        } else {
+            $mime = $mime[0];
+        }
+
+		return new Response($fs->read($inode), 200, [
+			"Content-Type" => $mime,
+		]);
+	}
 }
